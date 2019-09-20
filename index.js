0307774cd73e2b30f0ffebfc655c7e468418aaed
N4de <jaAnnala@gmail.com>
2019-09-20 16:03:49 +0300

    move blog model to models folder

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog');
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true , useUnifiedTopology: true})
