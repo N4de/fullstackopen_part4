7b4dafa1d6eed835ecaa92e4a7750bcb896b1e9e
N4de <jaAnnala@gmail.com>
2019-09-21 11:06:39 +0300

    move app logic to app.js

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog');
+const http = require('http');
+const app = require('./app'); // varsinainen Express-sovellus
+const config = require('./utils/config');
 
+const server = http.createServer(app);
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true , useUnifiedTopology: true})
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
 
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
