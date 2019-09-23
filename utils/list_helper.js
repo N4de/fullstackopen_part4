cdb9ef7254647f5889703749d08d9fd285f0c96e
N4de <jaAnnala@gmail.com>
2019-09-23 17:52:18 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => (1);
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  const likes = blogs.reduce((total, blog) => ({ likes: total.likes + blog.likes }));
+  return likes.likes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
