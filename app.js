0826dd1f999664a4cad22963d191b52f09461f84
N4de <jaAnnala@gmail.com>
2019-10-17 17:47:39 +0300

    add users router to app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const loginRouter = require('./controllers/login');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/login', loginRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
