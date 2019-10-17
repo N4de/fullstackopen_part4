d2be678ec4063aff015f6aecca0f10657a9499df
N4de <jaAnnala@gmail.com>
2019-10-17 17:48:17 +0300

    4.15: blogilistan laajennus, step4

--- models/user.js
+++ models/user.js
@@ -3,8 +3,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  username: String,
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+  ],
 });
 
 userSchema.set('toJSON', {
@@ -12,7 +19,11 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
   },
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
