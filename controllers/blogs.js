2a30541c4bfefff61ae259a62a7d79b921cc8163
N4de <jaAnnala@gmail.com>
2019-10-21 18:54:41 +0300

    4.20*: blogilistan laajennus, step9

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,16 +3,6 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user');
   response.json(blogs);
@@ -24,7 +14,7 @@ blogsRouter.post('/', async (request, response, next) => {
     blogBody.likes = 0;
   }
 
-  const token = getTokenFrom(request);
+  const { token } = request;
 
   try {
     const decodedToken = jwt.verify(token, process.env.SECRET);
--- controllers/login.js
+++ controllers/login.js
@@ -13,6 +13,7 @@ loginRouter.post('/', async (request, response) => {
     : await bcrypt.compare(body.password, user.passwordHash);
 
   if (!(user && passwordCorrect)) {
+    console.log('haloo!?!?!');
     response.status(401).json({
       error: 'invalid username or password',
     });
